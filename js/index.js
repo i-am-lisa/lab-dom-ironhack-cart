6f989f062a3f89c6bfdc9d1a63899cff870c87cf
Lisa Montebaur <lisamontebaur@gmail.com>
2021-01-19 21:04:24 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,23 +1,60 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  // assigning Variables
+  let price = product.querySelector('.price span');
+  let quantity = product.querySelector('.quantity input');
 
-  //... your code goes here
+  let subtotal = product.querySelector('.subtotal span');
+  //  console.log(price);
+  //  console.log(quantity);
+
+  let subtotalNum = Number(price.innerHTML) * Number(quantity.value);
+  //console.log(total);
+
+  
+  subtotal.innerHTML = subtotalNum;  
+  return subtotalNum;
+
+
+  //console.log('Calculating subtotal, yey!');
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let allProducts = document.querySelectorAll('.product');
 
+  allProducts.forEach(function(elem){
+    updateSubtotal(elem);
+  });
+  
   // ITERATION 3
-  //... your code goes here
+  let allSubtotals = document.querySelectorAll('.subtotal span');
+  //console.log(subtotals) ;
+
+  let totalValue = document.querySelector('#total-value span');
+  console.log(totalValue);
+  let total = 0;
+
+  allSubtotals.forEach(function(singleSubtotal){
+    total = total + Number(singleSubtotal.innerHTML); 
+    //console.log(singleSubtotal) ;
+  });
+
+    // console.log(total) ;
+
+
+    
+
+    // updating visually
+   totalValue.innerHTML = total;  
+    
 }
 
 // ITERATION 4
@@ -25,7 +62,9 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+
+  
+
 }
 
 // ITERATION 5
